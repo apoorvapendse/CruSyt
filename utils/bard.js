82f3297af8103415c27fcc701121bb87bbcb4442
Apoorva Pendse <102853901+apoorvapendse@users.noreply.github.com>
2023-11-17 18:07:03 +0530

    comments added for explanation

--- utils/bard.js
+++ utils/bard.js
@@ -6,6 +6,11 @@ export async function getResults(searchTerm) {
   const request = `curl -H "Content-Type: application/json" -d "{ \\"prompt\\": { \\"text\\": \\"Give me 50 ${searchTerm} reviews both good and bad in a json array with two field rating and review \\" } }" "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=AIzaSyBE-myxHyuWxQHJfIZXLYzILLVghP6nkhA"`;
   let reviews = [];
 
+// The await keyword can only be used with functions
+//  that return a Promise, and it seems that exec doesn't return a Promise.
+// To solve this issue, I'm 
+// wrapping the exec function in a Promise and res or rej
+//  the Promise based on the result of the execution
   return new Promise((resolve, reject) => {
     let child = exec(request, function (error, stdout, stderr) {
       if (error) {
@@ -18,11 +23,15 @@ export async function getResults(searchTerm) {
         let data = JSON.parse(stdout);
         if (data.candidates && Array.isArray(data.candidates) && data.candidates.length > 0) {
           let obj = data.candidates[0];
+          //making output json parsable
           if (obj.output && typeof obj.output === 'string') {
             obj.output = obj.output.replace("```", "").replace("```", "").replace("json", "");
             delete obj.safetyRatings;
             try {
               let jsonString = obj.output.replace(/\n/g, '');
+              //removing \n from the response string 
+
+              //making the output string into valid json format
               while (jsonString[jsonString.length - 1] != "}") {
                 jsonString = jsonString.slice(0, jsonString.length - 1);
               }
